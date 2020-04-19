20c34f9fd1cdebd86d49dec39efefe964f6ad4e7
peynaj <peynajber@gmail.com>
2020-04-19 10:24:39 +0430

    Fix jupyter convert and update version

--- setup.py
+++ setup.py
@@ -24,6 +24,7 @@ setup(
         'qbuild-jupyter @ git+https://github.com/peynaj/qbuild-jupyter',
         # TODO: switch to 'git+https://gitlab.com/codamooz/challenges/qbuild-jupyter' ;
         #       if this repos is completed and you have access to it.
+        # TODO: How to define version of qbuild-jupyter to install: '0.1.2'
     ],
     classifiers=[
         'Development Status :: 4 - Beta',
